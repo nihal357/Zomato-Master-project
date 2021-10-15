bd6fd13738748d6af6f5fd83e02d44810285aaaa
Nihal <nihalchaudhari357@gmail.com>
2021-10-16 02:43:34 +0530

    add delivery cards

--- client/src/Components/Delivery/index.js
+++ client/src/Components/Delivery/index.js
@@ -1,38 +1,39 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+
 
 // components
 import DeliveryCarousal from "./DeliveryCarousal";
 import Brand from "./Brand";
-import RestaurantCard from "../RestaurantCard";
+import RestaurantCard from "../RestaurantCard"
 
 const Delivery = () => {
-    const [restaurantList, setRestaurantList] = useState([]);
-
-    const reduxState = useSelector(
-        (globalStore) => globalStore.restaurant.restaurants
-    );
-
-    useEffect(() => {
-        reduxState.restaurants && setRestaurantList(reduxState.restaurants);
-    }, [reduxState.restaurants]);
-
+    // const [restaurantList, setRestaurantList] = useState([
+    //     {
+    //         _id: "1234567",
+    //         photos: ["https://b.zmtcdn.com/data/reviews_photos/c53/819476e0225ebc6b3debaa2c4fce0c53_1627552252.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"],
+    //         name: "Dacdenil's Burger",
+    //         isPro: true,
+    //         isOff: 80,
+    //         durationOfdelivery: 47,
+    //         restaurantReviewValue: 4,
+    //         cuisine: ["Burger", "Fast Food"],
+    //         averageCost: 100,
+    //     },
+    // ]);
     return (
         <>
             <DeliveryCarousal />
             {/* <Brand /> */}
-            <h1 className="text-xl mt-4 mb-2 md:mt-8 md:text-3xl md:font-semibold">
-                Delivery Restaurants in Vijay Nagar
-            </h1>
-            <div className="flex justify-between flex-wrap">
-                {restaurantList.map((restaurant) => (
-                    <RestaurantCard
-                        {...restaurant}
-                        key={restaurant._id}
-                        whereIsthisres="asf"
-                    />
-                ))}
-            </div>
+            {/* <div className="flex justify-between flex-wrap">
+                {
+                    restaurantList.map((restaurant) => {
+                        <RestaurantCard {...restaurant}
+                            key={restaurant._id}
+                        />
+                    })
+                }
+            </div> */}
+            <RestaurantCard />
         </>
     );
 };
--- client/src/Components/RestaurantCard.js
+++ client/src/Components/RestaurantCard.js
@@ -1,63 +1,44 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import React from "react";
 import { AiTwotoneStar } from "react-icons/ai";
-import { getImage } from "../Redux/Reducer/Image/Image.action";
 
 const RestaurantCard = (props) => {
-    const [image, setImage] = useState({
-        images: [],
-    });
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        props.photos &&
-            dispatch(getImage(props.photos)).then((data) =>
-                setImage(data.payload.image)
-            );
-    }, [props.photos]);
-
     return (
-        <Link to={`/restaurant/${props._id}`} className="w-full">
-            <div className="bg-white p-4 mb-4  w-full rounded-2xl transition duration-700 ease-in-out hover:shadow-lg md:w-1/2 lg:w-1/3">
+        <>
+            <div className="bg-white p-4 w-full mb-4 md:w-1/2 lg:w-1/3 transition duration-700 ease-in-out rounded-2xl hover:shadow-lg">
                 <div className="w-full h-56 lg:h-64 relative">
                     <div className="absolute w-full bottom-4 flex items-end justify-between">
                         <div className="flex flex-col gap-2 items-start">
-                            {props.isPro && (
-                                <span className="bg-zomato-400 text-white px-2 py-1 rounded text-sm">
-                                    Pro extra 10% off
-                                </span>
-                            )}
-                            {props.isOff && (
-                                <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">
-                                    ₹{`${props.isOff}`} OFF
-                                </span>
-                            )}
+
+                         
+                                <span className="bg-zomato-400 text-white px-2 py-1 rounded text-sm">Pro extra 10% off</span>
+                           
+
+
+                            
+                                <span className="bg-blue-600 text-white px-2 py-1 rounded text-sm">₹ 80 off</span>
+                            
+
                         </div>
-                        <span className="bg-white bg-opacity-75 p-1 rounded	mr-3">
-                            {props.durationOfdelivery} min
-                        </span>
+                        <span className="bg-white bg-opacity-75 p-1 rounded mr-3">44 min</span>
                     </div>
                     <img
-                        src={image.images.length && image.images[0].location}
+                        src="https://b.zmtcdn.com/data/pictures/chains/6/3300046/fc013e28cda53e1d3a11657c44bf3d41.jpg?fit=around|300:273&crop=300:273;*,*"
                         alt="food"
-                        className="w-full h-full rounded-2xl"
+                        className="w-full h-full rounded-lg"
                     />
                 </div>
                 <div className="my-2 flex flex-col gap-2">
                     <div className="flex items-center justify-between">
-                        <h4 className="text-xl font-medium">{props.name}</h4>
-                        <span className="bg-green-800 text-white text-sm p-1 rounded flex items-center">
-                            {props.restaurantReviewValue} <AiTwotoneStar />
-                        </span>
+                        <h4 className="text-xl font-medium">Panino The Sandwich World</h4>
+                        <span className="bg-green-800 text-white flex items-center text-sm p-1 rounded">3.7 <AiTwotoneStar /></span>
                     </div>
                     <div className="flex items-center justify-between text-gray-500">
-                        <p>{props.cuisine.join(", ")}</p>
-                        <p>₹ {props.averageCost} for one</p>
+                        <p>Sandwich, Fast Food, Beverages, Shake</p>
+                        <p>₹ 150 for one</p>
                     </div>
                 </div>
             </div>
-        </Link>
+        </>
     );
 };
 

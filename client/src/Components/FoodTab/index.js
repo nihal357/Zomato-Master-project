4325e942225dbf1d4d4998c72383407089b46026
Nihal <nihalchaudhari357@gmail.com>
2021-10-15 22:54:30 +0530

    add active food tab

--- client/src/Components/FoodTab/index.js
+++ client/src/Components/FoodTab/index.js
@@ -1,28 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
 import { RiShoppingBag3Line } from "react-icons/ri";
 import { IoFastFoodOutline, IoNutritionOutline } from "react-icons/io5";
 import { BiDrink } from "react-icons/bi";
 
 const MobileTabs = () => {
+    const [allTypes, setAllTypes] = useState([
+        {
+            id: "delivery",
+            icon: <RiShoppingBag3Line />,
+            name: "Delivery",
+        },
+        {
+            id: "dining",
+            icon: <IoFastFoodOutline />,
+            name: "Dining Out",
+        },
+        {
+            id: `night`,
+            icon: <BiDrink />,
+            name: "Night life",
+        },
+        {
+            id: `nutri`,
+            icon: <IoNutritionOutline />,
+            name: "Nutrition",
+        },
+    ]);
+    const { type } = useParams();
+
     return (
         <>
-            <div className="lg:hidden bg-white shadow-lg p-3 fixed bottom-0 z-10 w-full flex items-center justify-between text-gray-500 border md:justify-evenly">
-                <div className="flex flex-col items-center text-xl">
-                    <RiShoppingBag3Line />
-                    <h5 className="text-sm">Delivery</h5>
-                </div>
-                <div className="flex flex-col items-center text-xl">
-                    <IoFastFoodOutline />
-                    <h5 className="text-sm">Dining Out</h5>
-                </div>
-                <div className="flex flex-col items-center text-xl">
-                    <BiDrink />
-                    <h5 className="text-sm">Night life</h5>
-                </div>
-                <div className="flex flex-col items-center text-xl">
-                    <IoNutritionOutline />
-                    <h5 className="text-sm">Nutrition</h5>
-                </div>
+            <div className="lg:hidden bg-white shadow-lg p-3 fixed bottom-0 z-10 w-full flex items-center justify-between md:justify-evenly text-gray-500 border">
+                {allTypes.map((items) => (
+                    <Link to={`/${items.id}`}>
+                        <div
+                            className={
+                                type === items.id
+                                    ? "flex flex-col relative items-center text-xl text-zomato-400 "
+                                    : "flex flex-col items-center text-xl "
+                            }
+                        >
+                            <div
+                                className={
+                                    type === items.id &&
+                                    "absolute -top-3 w-full h-2 border-t-2 border-zomato-400"
+                                }
+                            />
+                            {items.icon}
+                            <h5 className="text-sm">{items.name}</h5>
+                        </div>
+                    </Link>
+                ))}
             </div>
         </>
     );
@@ -34,8 +63,8 @@ const LargeTabs = () => {
             <div className="hidden lg:flex gap-12 container px-40 mx-auto">
                 <div className="flex items-center gap-3">
                     <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                        <img 
-                            src= "https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png"
+                        <img
+                            src="https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png"
                             alt="delivery"
                             className="w-full h-full"
                         />
@@ -44,7 +73,7 @@ const LargeTabs = () => {
                 </div>
                 <div className="flex items-center gap-3">
                     <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                        <img 
+                        <img
                             src="https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png"
                             alt="delivery"
                             className="w-full h-full"
@@ -54,7 +83,7 @@ const LargeTabs = () => {
                 </div>
                 <div className="flex items-center gap-3">
                     <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                        <img 
+                        <img
                             src="https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png"
                             alt="delivery"
                             className="w-full h-full"
@@ -64,7 +93,7 @@ const LargeTabs = () => {
                 </div>
                 <div className="flex items-center gap-3">
                     <div className="w-16 h-16 bg-gray-100 p-4 rounded-full">
-                        <img 
+                        <img
                             src="https://b.zmtcdn.com/data/o2_assets/54cad8274d3c3ec7129e0808a13b27c31616582882.png"
                             alt="delivery"
                             className="w-full h-full"
@@ -80,13 +109,13 @@ const LargeTabs = () => {
 
 const FoodTab = () => {
     return (
-      <>
-        <div>
-          <MobileTabs />
-          <LargeTabs />
-        </div>
-      </>
+        <>
+            <div>
+                <MobileTabs />
+                <LargeTabs />
+            </div>
+        </>
     );
-  };
-  
-  export default FoodTab;
+};
+
+export default FoodTab;

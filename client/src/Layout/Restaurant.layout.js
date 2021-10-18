1e94ac8c4a3771dc154cc7ab8a150e95547f112a
Nihal <nihalchaudhari357@gmail.com>
2021-10-18 12:54:20 +0530

    Restaurant Layout buttons done

--- client/src/Layout/Restaurant.layout.js
+++ client/src/Layout/Restaurant.layout.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import { TiStarOutline } from "react-icons/ti";
+import { RiDirectionLine, RiShareForwardLine } from "react-icons/ri";
+import { BiBookmarkPlus } from "react-icons/bi";
 
 //component
 import RestaurantNavbar from "../Components/Navbar/restaurantNavbar";
 import ImageGrid from '../Components/restaurant/ImageGrid';
+import InfoButtons from '../Components/restaurant/InfoButtons';
 import RestaurantInfo from '../Components/restaurant/RestaurantInfo';
+
 
 const RestaurantLayout = () => {
     return (
@@ -24,6 +28,20 @@ const RestaurantLayout = () => {
                     deliveryRating="3.5"
                     cuisine="Biryani"
                     address="Sadar, Nagpur" />
+                <div className="my-4 flex flex-wrap gap-3">
+                    <InfoButtons isActive>
+                        <TiStarOutline /> Add Review
+                    </InfoButtons>
+                    <InfoButtons>
+                        <RiDirectionLine /> Direction
+                    </InfoButtons>
+                    <InfoButtons>
+                        <BiBookmarkPlus /> Bookmark
+                    </InfoButtons>
+                    <InfoButtons>
+                        <RiShareForwardLine /> Share
+                    </InfoButtons>
+                </div>
             </div>
         </>
     )

b32c2429adbf8e89a81ba5a76409fe6119bedc63
Nihal <nihalchaudhari357@gmail.com>
2021-10-19 11:34:42 +0530

    add float menu

---
+++ client/src/Components/restaurant/Order-Online/MenuListContainer.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import React from 'react';
+
+// components
+import MenuCategory from "./MenuCategory";
+
+const MenuListContainer = () => {
+    const [selected, setSelected] = useState("");
+    const onClickHandler = (e) => {
+        if (e.target.id) {
+            setSelected(e.target.id);
+        }
+        return;
+    };
+    return (
+        <>
+            <div className="w-full flex flex-col gap-3">
+                <MenuCategory
+                    name="Recommended"
+                    items={["", ""]}
+                    onClickHandler={onClickHandler}
+                    isActive={selected === "Recommended"}
+                />
+            </div>
+        </>
+    );
+};
+
+export default MenuListContainer;

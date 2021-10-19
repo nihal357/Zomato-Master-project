d034f06c0ffc410539dd92e0de79e7761448888a
Nihal <nihalchaudhari357@gmail.com>
2021-10-19 22:57:20 +0530

    updated backend

--- database/food/index.js
+++ database/food/index.js
@@ -21,7 +21,11 @@ const FoodSchema = new mongoose.Schema({
     type: mongoose.Types.ObjectId,
     ref: "Restaurants",
     required: true
-  }
+  },
+  reviews: {
+    type: mongoose.Types.ObjectId,
+    ref: "Reviews",
+  },
 },
 {
   timestamps: true
